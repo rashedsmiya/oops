be1ae425fe8d5f84b8db3b97846a3b3d0c3e362f
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2026-01-08 09:18:28 +0600

    Initial Commit

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
 
-    class Father {
-       public const mark = 101;
-        function disp() {
-            echo self::mark;
-        }
-
-    }
-
-echo Father::mark;
+     const ANIMALS = array("cat", "dog", "ww");
+     echo  ANIMALS[1];
