969d70c64b9d79da1e1979782a4a8b5ddc978089
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2026-01-08 12:29:41 +0600

    Initial Commit

--- index.php
+++ index.php
@@ -1,4 +1,34 @@
 <?php
 
-     const ANIMALS = array("cat", "dog", "ww");
-     echo  ANIMALS[1];
+        interface Animal {
+            public function makeSound();
+        }
+
+        class Cat implements Animal {
+            public function makeSound() {
+                echo "Meow";
+            }
+        }
+
+        class Dog implements Animal {
+            public function makeSound(){
+                echo "Bark";
+            }
+        }
+
+        class Mouse implements Animal {
+            public function makeSound(){
+                echo " Squeak";
+            }
+        }
+
+        $cat = new Cat();
+        $dog = new Dog();
+        $mouse = new Mouse();
+        $animals = array($cat, $dog, $mouse);
+
+        // Tell the animals to make a sound
+        foreach($animals as $animal) {
+            $animal->makeSound();
+        }
+
