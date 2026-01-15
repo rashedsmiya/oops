844a34b82178f9c7185f5a20ee03c6edaa9cd97f
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2026-01-15 12:50:07 +0600

    adds

--- Object-Oriented/Constructor.php
+++ Object-Oriented/Constructor.php
@@ -3,26 +3,30 @@
         public $a;
         public $b;
 
-        public function __construct($first, $second){
+        public function __construct($first, $second = 15){
             $this->a = $first;
             $this->b = $second;
+
+            $this->display();
         }
         public function sum(){
             return $this->a + $this->b;
         }
-
-        public function mul(){
-            return $this->a * $this->b;
+        public function mul($c = 2){
+            return $this->a * $this->b * $c;
+        }
+        public function display(){
+            echo "welcome Boss <br>";
         }
     }
 
     $foo = new Math(10, 20);
     echo $foo->sum();
     echo "<br>";
-    $obj = new Math(40, 2);
+    $obj = new Math(40);
     echo $obj->sum();
     echo "<br>";
-    echo $obj-> Mul();
+    echo $obj->mul(3);
 
 
 
