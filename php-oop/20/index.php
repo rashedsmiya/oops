8c665f484ae8e86fdb0a0f06a1c3a7891c42df5a
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2026-01-18 09:59:47 +0600

    a

--- php-oop/20/index.php
+++ php-oop/20/index.php
@@ -1,8 +1,12 @@
 <?php 
      
        require "user.php";
+       require "student.php";
 
        use App\User;
 
        $user = new User;
        $user->get();
+
+       $user2 = new School\User;
+       $user2->get();
--- php-oop/20/student.php
+++ php-oop/20/student.php
@@ -1,3 +1,17 @@
 <?php 
 
-     
+     namespace School;
+
+     class User 
+     {
+        public function get(){
+            echo "This is get of student file <br>";
+        }
+     }
+
+     class Student 
+     {
+        public function display(){
+            echo "Welcome to Student class.<br>";
+        }
+     }
