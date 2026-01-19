42631de1a588fa40fb2e1fc463048b35cd4768fa
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2026-01-19 19:13:58 +0600

    a

--- play-with-oop/02/index.php
+++ play-with-oop/02/index.php
@@ -3,12 +3,21 @@
     require 'vendor/autoload.php';
 
     Use App\Student;
+    use App\Teacher;
+    use App\Staf;
     Use App\Main;
     use App\Model\User;
 
+    $std = new Student;
 
-    $user = new User(); 
-    $main = new Main($user);
+    $staf = new Staf;
+
+    $main = new Main($staf);
     $main->hello();
+
+
+    // $user = new User(); 
+    // $main = new Main($user);
+    // $main->hello();
 
     
---
+++ play-with-oop/02/src/app/staf.php
@@ -0,0 +1,12 @@
+<?php 
+
+    namespace App;
+
+    use App\Model\User;
+
+    class Staf extends User
+    {
+        public function test(){
+            echo "This is Test";
+        }
+    }
