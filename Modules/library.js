08587aa7ab932a73a267f3ecc0a321842cac945c
Rashed Miya <rashedmiyaofficial@gmail.com>
2026-02-17 04:12:17 +0600

    a

---
+++ Modules/library.js
@@ -0,0 +1,16 @@
+
+
+// let message = "ES6 Modules";
+
+// function user(name) {
+//     console.log("Hello " + name);
+//     return "Hello " + name;
+// }
+
+// class test {
+//     constructor() {
+//         console.log("Test class");
+//     }
+// }
+
+// export {message, user, test};
